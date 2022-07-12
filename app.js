a4ef8dc2fb15c526d9f6333c37ebdbcbb4b1459d
Mohammad Hammad Ahmad <hammad.ahmad2000@gmail.com>
2022-07-12 13:10:23 +0500

    Added my project

---
+++ app.js
@@ -0,0 +1,8 @@
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookied for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
